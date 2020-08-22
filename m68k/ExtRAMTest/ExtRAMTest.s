cd9a1ee12ba5754bb21a98410fa7b1dd0f2b8fea
Doug Gilliland <doug@douglasgilliland.com>
2020-08-22 06:59:29 -0400

    Create ExtRAMTest.s

---
+++ m68k/ExtRAMTest/ExtRAMTest.s
@@ -0,0 +1,18 @@
+* Test External SRAM
+* External SRAM on the RETRO-EP4CE15 card goes from 0x300000 to 0x3FFFFF (1 MB)
+* External SRAM only supports 8-bit accesses
+* TUTOR14 uses SRAM from 0x000000 to 0x000800
+
+RAMSTART	= 0X300000
+RAMEND		= 0X3FFFFF
+
+* Code follows
+
+	.ORG    0x001000
+
+STARTTEST:
+	MOVE.L  #0x00300000,A0
+	MOVE.L  #0X00000055,D0
+	MOVE.B	D0,(A0)
+	RTS
+	
