047b64e563a7942eec73bbbda4f8a32bbf05fc4c
Doug G <doug@douglasgilliland.com>
2020-08-28 13:16:00 -0400

    testing testser code

--- m68k/testser/testser.i
+++ m68k/testser/testser.i
@@ -2,16 +2,40 @@
 # 1 "<built-in>"
 # 1 "<command-line>"
 # 1 "testser.c"
-
-
-
-
+# 10 "testser.c"
+void printCharToACIA(unsigned char);
+void printStringToACIA(const char *);
+void printCharToVDU(unsigned char);
+void printStringToVDU(const char *);
 
 void _start(void)
 {
- unsigned char aciaStat;
- do
-  aciaStat = *(unsigned char *) 0x010041;
- while ((aciaStat & 0x2) == 0x2);
- *(unsigned char *) 0x010043 = 'X';
+ printStringToACIA("Test String 1");
+ printStringToVDU("Test String 2");
+}
+
+void printCharToACIA(unsigned char charToPrint)
+{
+ while ((*(volatile unsigned char *) 0x010041 & 0x2) == 0x0);
+ * (volatile unsigned char *) 0x010043 = charToPrint;
+}
+
+void printStringToACIA(const char * strToPrint)
+{
+    int strOff = 0;
+    while(strToPrint[strOff] != 0)
+        printCharToACIA(strToPrint[strOff]);
+}
+
+void printCharToVDU(unsigned char charToPrint)
+{
+ while ((*(volatile unsigned char *) 0x010040 & 0x2) == 0x0);
+ * (volatile unsigned char *) 0x010042 = charToPrint;
+}
+
+void printStringToVDU(const char * strToPrint)
+{
+    int strOff = 0;
+    while(strToPrint[strOff] != 0)
+        printCharToVDU(strToPrint[strOff]);
 }
--- m68k/testser/testser.s
+++ m68k/testser/testser.s
@@ -1,24 +1,131 @@
 #NO_APP
 	.file	"testser.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Test String 1"
+.LC1:
+	.string	"Test String 2"
+	.text
 	.align	2
 	.globl	_start
 	.type	_start, @function
 _start:
-	link.w %fp,#-4
-.L2:
-	move.l #65601,%a0
-	move.b (%a0),-1(%fp)
-	moveq #0,%d0
-	move.b -1(%fp),%d0
-	moveq #2,%d1
-	and.l %d1,%d0
-	tst.l %d0
-	jne .L2
-	move.l #65603,%a0
-	move.b #88,(%a0)
+	link.w %fp,#0
+	pea .LC0
+	jsr printStringToACIA
+	addq.l #4,%sp
+	pea .LC1
+	jsr printStringToVDU
+	addq.l #4,%sp
 	nop
 	unlk %fp
 	rts
 	.size	_start, .-_start
+	.align	2
+	.globl	printCharToACIA
+	.type	printCharToACIA, @function
+printCharToACIA:
+	link.w %fp,#-4
+	move.l 8(%fp),%d0
+	move.b %d0,%d0
+	move.b %d0,-2(%fp)
+	nop
+.L3:
+	move.l #65601,%a0
+	move.b (%a0),%d0
+	move.b %d0,%d0
+	and.l #255,%d0
+	moveq #2,%d1
+	and.l %d1,%d0
+	tst.l %d0
+	jeq .L3
+	move.l #65603,%a0
+	move.b -2(%fp),(%a0)
+	nop
+	unlk %fp
+	rts
+	.size	printCharToACIA, .-printCharToACIA
+	.align	2
+	.globl	printStringToACIA
+	.type	printStringToACIA, @function
+printStringToACIA:
+	link.w %fp,#-4
+	clr.l -4(%fp)
+	jra .L5
+.L6:
+	move.l -4(%fp),%d0
+	move.l 8(%fp),%a0
+	add.l %d0,%a0
+	move.b (%a0),%d0
+	move.b %d0,%d0
+	and.l #255,%d0
+	move.l %d0,-(%sp)
+	jsr printCharToACIA
+	addq.l #4,%sp
+.L5:
+	move.l -4(%fp),%d0
+	move.l 8(%fp),%a0
+	add.l %d0,%a0
+	move.b (%a0),%d0
+	tst.b %d0
+	jne .L6
+	nop
+	nop
+	unlk %fp
+	rts
+	.size	printStringToACIA, .-printStringToACIA
+	.align	2
+	.globl	printCharToVDU
+	.type	printCharToVDU, @function
+printCharToVDU:
+	link.w %fp,#-4
+	move.l 8(%fp),%d0
+	move.b %d0,%d0
+	move.b %d0,-2(%fp)
+	nop
+.L8:
+	move.l #65600,%a0
+	move.b (%a0),%d0
+	move.b %d0,%d0
+	and.l #255,%d0
+	moveq #2,%d1
+	and.l %d1,%d0
+	tst.l %d0
+	jeq .L8
+	move.l #65602,%a0
+	move.b -2(%fp),(%a0)
+	nop
+	unlk %fp
+	rts
+	.size	printCharToVDU, .-printCharToVDU
+	.align	2
+	.globl	printStringToVDU
+	.type	printStringToVDU, @function
+printStringToVDU:
+	link.w %fp,#-4
+	clr.l -4(%fp)
+	jra .L10
+.L11:
+	move.l -4(%fp),%d0
+	move.l 8(%fp),%a0
+	add.l %d0,%a0
+	move.b (%a0),%d0
+	move.b %d0,%d0
+	and.l #255,%d0
+	move.l %d0,-(%sp)
+	jsr printCharToVDU
+	addq.l #4,%sp
+.L10:
+	move.l -4(%fp),%d0
+	move.l 8(%fp),%a0
+	add.l %d0,%a0
+	move.b (%a0),%d0
+	tst.b %d0
+	jne .L11
+	nop
+	nop
+	unlk %fp
+	rts
+	.size	printStringToVDU, .-printStringToVDU
 	.ident	"GCC: (GNU) 9.3.1 20200817"
