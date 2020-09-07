c414ef8bed813da87db96d4dbde839d2c9e3b99c
Doug Gilliland <doug@douglasgilliland.com>
2020-09-07 12:48:59 -0400

    add comments

--- TG68/Apps/Rectangles/rectangles.c
+++ TG68/Apps/Rectangles/rectangles.c
@@ -1,3 +1,11 @@
+// rectangles.c - Draws rectangles on the VGA screen
+// random size rectangles
+// combined with previous rectangle colors
+// scrolls screen vertically as well
+// scrolls on vert sync interrupt
+// very busy screen
+// bootloader leaves debug window up
+
 #include "vga.h"
 #include "ints.h"
 #include "uart.h"
@@ -5,11 +13,12 @@
 
 short *FrameBuffer;
 
-extern short pen;
+extern short pen;		// no used in this function - set in draw
 extern void DrawIteration();
 
 static short framecount=0;
 
+// scrolls in vert blank
 void vblank_int()
 {
 	int yoff;
@@ -45,4 +54,3 @@ int main(int argc,char **argv)
 		DrawIteration();
 	}
 }
-
