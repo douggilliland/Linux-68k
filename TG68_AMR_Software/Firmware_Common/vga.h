c9d5d57bdf224d61c1f50d29655933efe1d79308
Doug Gilliland <doug@douglasgilliland.com>
2020-09-19 13:29:00 -0400

    Update vga.h

--- TG68_AMR_Software/Firmware_Common/vga.h
+++ TG68_AMR_Software/Firmware_Common/vga.h
@@ -13,15 +13,15 @@
 #define HW_VGA_L(x) *(volatile unsigned long *)(VGABASE+x)
 #define VGA_INT_VBLANK 1
 
-#define REG_VGA_HTOTAL 0x08
-#define REG_VGA_HSIZE 0x0a
-#define REG_VGA_HBSTART 0x0c
-#define REG_VGA_HBSTOP 0x0e
-#define REG_VGA_VTOTAL 0x10
-#define REG_VGA_VSIZE 0x12
-#define REG_VGA_VBSTART 0x14
-#define REG_VGA_VBSTOP 0x16
-#define REG_VGA_CONTROL 0x18
+#define REG_VGA_HTOTAL 0x08		// offset to register for total number of pixel clocks in a scanline 
+#define REG_VGA_HSIZE 0x0a		// offset to register for number of horizontal pixels displayed
+#define REG_VGA_HBSTART 0x0c	// offset to register for start of the horizontal blank
+#define REG_VGA_HBSTOP 0x0e		// offset to register for end of the horizontal blanking period
+#define REG_VGA_VTOTAL 0x10		// offset to register for number of scanlines in a frame
+#define REG_VGA_VSIZE 0x12		// offset to register for number of displayed scanlines
+#define REG_VGA_VBSTART 0x14	// offset to register for start of the vertical blanking period
+#define REG_VGA_VBSTOP 0x16		// offset to register for end of the vertical blanking period
+#define REG_VGA_CONTROL 0x18	// offset to register for 
 
 #define BIT_VGA_CONTROL_OVERLAY 7
 #define FLAG_VGA_CONTROL_OVERLAY (1<<BIT_VGA_CONTROL_OVERLAY)
@@ -36,7 +36,7 @@
 #define MASK_VGA_CONTROL_PIXELCLOCK_7 0xc
 #define MASK_VGA_CONTROL_PIXELCLOCK_8 0xe
 
-#define VGACHARBUFFERBASE 0x80000800
+#define VGACHARBUFFERBASE 0x80000800	// character overlay buffer
 extern char *VGACharBuffer;
 
 enum VGA_ScreenModes {
