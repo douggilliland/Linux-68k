06bd72e41c4120ffa0a62022532d6af3a7eb38db
Doug Gilliland <doug@douglasgilliland.com>
2021-01-24 04:05:24 -0500

    Update spi_readsector.s

--- TG68_AMR_Software/Apps/Demo/spi_readsector.s
+++ TG68_AMR_Software/Apps/Demo/spi_readsector.s
@@ -3,7 +3,7 @@ SPI_PUMP	equ	$81000100
 	XDEF spi_readsector
 spi_readsector
 	move.l	4(a7),a0
-	movem.l	d2-7/a2,-(a7)
+	movem.l	d2-d7/a2,-(a7)
 
 	moveq	#15,d7
 	lea	SPI_PUMP,a1
@@ -14,5 +14,5 @@ spi_readsector
 	add.l	#32,a0
 	dbf	d7,.loop
 
-	movem.l	(a7)+,d2-7/a2
+	movem.l	(a7)+,d2-d7/a2
 	rts
