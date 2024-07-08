f425a65d50d61db75766ed4e9b0031f1f8800772
Doug Gilliland <doug@douglasgilliland.com>
2024-07-08 17:54:38 -0400

    Update S68K_004.c

--- S68K_004/S68K_004.c
+++ S68K_004/S68K_004.c
@@ -8,10 +8,10 @@ int main(void)
 	unsigned char * DUART_OPC;
 	unsigned char * DUART_OPS;
 	unsigned char * DUART_OPR;
-	DUART_BASE = (unsigned long *) 0x0F0000; 	/* Base Addr of DUART		*/
-	DUART_OPC  = (unsigned long *) 0x0F0026;	/* Output port config (W)	*/
-	DUART_OPS  = (unsigned long *) 0x0F0028;  /* Output port Set (W)		*/
-	DUART_OPR  = (unsigned long *) 0x0F0030;  /* Output port Clear (W)	*/
+	DUART_BASE = (unsigned char *) 0x0F0000; 	/* Base Addr of DUART		*/
+	DUART_OPC  = (unsigned char *) 0x0F0026;	/* Output port config (W)	*/
+	DUART_OPS  = (unsigned char *) 0x0F0028;  /* Output port Set (W)		*/
+	DUART_OPR  = (unsigned char *) 0x0F0030;  /* Output port Clear (W)	*/
 	
 	*DUART_OPC = (char) 0x00;
 	*DUART_OPR = (char) 0xFC;
