b13211453b4a7194cc19fee56f71019c59d57a65
Doug Gilliland <doug@douglasgilliland.com>
2024-07-08 17:47:15 -0400

    Update S68K_004.c

--- S68K_004/S68K_004.c
+++ S68K_004/S68K_004.c
@@ -13,16 +13,16 @@ int main(void)
 	DUART_OPS  = 0x0F0028;  /* Output port Set (W)		*/
 	DUART_OPR  = 0x0F0030;  /* Output port Clear (W)	*/
 	
-	*DUART_OPC = 0x00;
-	*DUART_OPR = 0xFC;
-	*DUART_OPS = 0x04;
+	(unsigned char *)DUART_OPC = 0x00;
+	(unsigned char *)DUART_OPR = 0xFC;
+	(unsigned char *)DUART_OPS = 0x04;
 	
 	while (1)
 	{
 		wait1Sec();
-		*DUART_OPR = 0xFC;
+		(unsigned char *)DUART_OPR = 0xFC;
 		wait1Sec();
-		*DUART_OPS = 0xFC;
+		(unsigned char *)DUART_OPS = 0xFC;
 	}
 }
 
