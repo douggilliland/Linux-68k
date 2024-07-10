720a9115f2087a10916be067947eb092a2c2bd25
Doug Gilliland <doug@douglasgilliland.com>
2024-07-10 15:23:53 -0400

    DUART defines

--- SIMPLE_68008/S68K_005/SIMPLE_68008.h
+++ SIMPLE_68008/S68K_005/SIMPLE_68008.h
@@ -1,5 +1,5 @@
 /* */
 
-unsigned char * DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
-unsigned char * DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
-unsigned char * DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
+#define DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
+#define DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
+#define DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
