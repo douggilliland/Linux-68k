e6bd53caed565d1d347d462d1e3cbaf076940917
Doug G <doug@douglasgilliland.com>
2024-07-10 14:53:43 -0400

    stuff

---
+++ SIMPLE_68008/S68K_005/S68K_005.s
@@ -0,0 +1,62 @@
+#NO_APP
+	.file	"S68K_005.c"
+	.text
+	.globl	DUART_OPC
+	.data
+	.align	2
+	.type	DUART_OPC, @object
+	.size	DUART_OPC, 4
+DUART_OPC:
+	.long	983066
+	.globl	DUART_OPS
+	.align	2
+	.type	DUART_OPS, @object
+	.size	DUART_OPS, 4
+DUART_OPS:
+	.long	983068
+	.globl	DUART_OPR
+	.align	2
+	.type	DUART_OPR, @object
+	.size	DUART_OPR, 4
+DUART_OPR:
+	.long	983070
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	link.w %fp,#-12
+	move.l #983066,-4(%fp)
+	move.l #983068,-8(%fp)
+	move.l #983070,-12(%fp)
+	move.l -4(%fp),%a0
+	clr.b (%a0)
+	move.l -12(%fp),%a0
+	move.b #-4,(%a0)
+.L2:
+	jsr wait1Sec
+	move.l -8(%fp),%a0
+	move.b #4,(%a0)
+	jsr wait1Sec
+	move.l -12(%fp),%a0
+	move.b #4,(%a0)
+	jra .L2
+	.size	main, .-main
+	.align	2
+	.globl	wait1Sec
+	.type	wait1Sec, @function
+wait1Sec:
+	link.w %fp,#-4
+	move.l #50000,-4(%fp)
+	jra .L4
+.L5:
+	subq.l #1,-4(%fp)
+.L4:
+	tst.l -4(%fp)
+	jne .L5
+	nop
+	nop
+	unlk %fp
+	rts
+	.size	wait1Sec, .-wait1Sec
+	.ident	"GCC: (GNU) 9.3.1 20200817"
