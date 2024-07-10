3e52d9b02d40255321e8d509da200fd2c892ce02
Doug G <doug@douglasgilliland.com>
2024-07-10 15:29:07 -0400

    addrs

--- SIMPLE_68008/S68K_005/S68K_005.s
+++ SIMPLE_68008/S68K_005/S68K_005.s
@@ -1,44 +1,24 @@
 #NO_APP
 	.file	"S68K_005.c"
 	.text
-	.globl	DUART_OPC
-	.data
-	.align	2
-	.type	DUART_OPC, @object
-	.size	DUART_OPC, 4
-DUART_OPC:
-	.long	983066
-	.globl	DUART_OPS
-	.align	2
-	.type	DUART_OPS, @object
-	.size	DUART_OPS, 4
-DUART_OPS:
-	.long	983068
-	.globl	DUART_OPR
-	.align	2
-	.type	DUART_OPR, @object
-	.size	DUART_OPR, 4
-DUART_OPR:
-	.long	983070
-	.text
 	.align	2
 	.globl	main
 	.type	main, @function
 main:
-	link.w %fp,#0
-	move.l #983066,DUART_OPC
-	move.l #983068,DUART_OPS
-	move.l #983070,DUART_OPR
-	move.l DUART_OPC,%a0
+	link.w %fp,#-12
+	move.l #983066,-4(%fp)
+	move.l #983068,-8(%fp)
+	move.l #983070,-12(%fp)
+	move.l -4(%fp),%a0
 	clr.b (%a0)
-	move.l DUART_OPR,%a0
+	move.l -12(%fp),%a0
 	move.b #-4,(%a0)
 .L2:
 	jsr wait1Sec
-	move.l DUART_OPS,%a0
+	move.l -8(%fp),%a0
 	move.b #4,(%a0)
 	jsr wait1Sec
-	move.l DUART_OPR,%a0
+	move.l -12(%fp),%a0
 	move.b #4,(%a0)
 	jra .L2
 	.size	main, .-main
