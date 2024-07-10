a71e5979b3290dab17e938840d2b76fd7c7cd560
Doug Gilliland <doug@douglasgilliland.com>
2024-07-10 15:28:29 -0400

    assign addrs

--- SIMPLE_68008/S68K_005/S68K_005.c
+++ SIMPLE_68008/S68K_005/S68K_005.c
@@ -9,9 +9,9 @@ int main(void)
 	unsigned char * DUART_OPC;
 	unsigned char * DUART_OPS;
 	unsigned char * DUART_OPR;
-	DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
-	DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
-	DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
+	DUART_OPC  = (unsigned char *) DUART_OPC_ADR;	/* Output port config (W)	*/
+	DUART_OPS  = (unsigned char *) DUART_OPS_ADR;	/* Output port Set (W)		*/
+	DUART_OPR  = (unsigned char *) DUART_OPR_ADR;	/* Output port Clear (W)	*/
 	
 	*DUART_OPC = (char) 0x00;
 	*DUART_OPR = (char) 0xFC;
--- SIMPLE_68008/S68K_005/SIMPLE_68008.h
+++ SIMPLE_68008/S68K_005/SIMPLE_68008.h
@@ -1,5 +1,5 @@
 /* */
 
-#define DUART_OPC (unsigned char *) 0x0F001A	/* Output port config (W)	*/
-#define DUART_OPS (unsigned char *) 0x0F001C	/* Output port Set (W)		*/
-#define DUART_OPR (unsigned char *) 0x0F001E	/* Output port Clear (W)	*/
+#define DUART_OPC_ADR 0x0F001A	/* Output port config (W)	*/
+#define DUART_OPS_ADR 0x0F001C	/* Output port Set (W)		*/
+#define DUART_OPR_ADR 0x0F001E	/* Output port Clear (W)	*/
