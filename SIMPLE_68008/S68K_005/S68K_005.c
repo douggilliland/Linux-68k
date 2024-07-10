a817394da8c3c831be825c54e9cac3fe875f492b
Doug Gilliland <doug@douglasgilliland.com>
2024-07-10 14:26:50 -0400

    more file stuff

---
+++ SIMPLE_68008/S68K_005/S68K_005.c
@@ -0,0 +1,33 @@
+/* */
+
+#include "SIMPLE_68008.h"
+
+void wait1Sec(void);
+
+int main(void)
+{
+	unsigned char * DUART_OPC;
+	unsigned char * DUART_OPS;
+	unsigned char * DUART_OPR;
+	DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
+	DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
+	DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
+	
+	*DUART_OPC = (char) 0x00;
+	*DUART_OPR = (char) 0xFC;
+	
+	while (1)
+	{
+		wait1Sec();
+		*DUART_OPS = (char) 0x04;
+		wait1Sec();
+		*DUART_OPR = (char) 0x04;
+	}
+}
+
+void wait1Sec(void)
+{
+	unsigned long loopCt = 50000;
+	while (loopCt > 0)
+		loopCt -= 1;
+}
---
+++ SIMPLE_68008/S68K_005/SIMPLE_68006.h
@@ -0,0 +1,5 @@
+/* */
+
+DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
+DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
+DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
