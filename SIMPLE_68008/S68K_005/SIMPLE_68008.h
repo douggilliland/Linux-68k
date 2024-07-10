70b7c09e776ee26fd62fb28e26d28cb5ce664ee1
Doug Gilliland <doug@douglasgilliland.com>
2024-07-10 14:59:14 -0400

    Update SIMPLE_68008.h

--- SIMPLE_68008/S68K_005/SIMPLE_68008.h
+++ SIMPLE_68008/S68K_005/SIMPLE_68008.h
@@ -1,5 +1,5 @@
 /* */
 
-DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
-DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
-DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
+unsigned char * DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
+unsigned char * DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
+unsigned char * DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
