b81bf0a61efd4907fe022f75070d5d3bfddd2f33
Doug Gilliland <doug@douglasgilliland.com>
2024-07-10 12:12:34 -0400

    Fix DUART addresses

--- S68K_004/S68K_004.c
+++ S68K_004/S68K_004.c
@@ -7,9 +7,9 @@ int main(void)
 	unsigned char * DUART_OPC;
 	unsigned char * DUART_OPS;
 	unsigned char * DUART_OPR;
-	DUART_OPC  = (unsigned char *) 0x0F0026;	/* Output port config (W)	*/
-	DUART_OPS  = (unsigned char *) 0x0F0028;	/* Output port Set (W)		*/
-	DUART_OPR  = (unsigned char *) 0x0F0030;	/* Output port Clear (W)	*/
+	DUART_OPC  = (unsigned char *) 0x0F001A;	/* Output port config (W)	*/
+	DUART_OPS  = (unsigned char *) 0x0F001C;	/* Output port Set (W)		*/
+	DUART_OPR  = (unsigned char *) 0x0F001E;	/* Output port Clear (W)	*/
 	
 	*DUART_OPC = (char) 0x00;
 	*DUART_OPR = (char) 0xFC;
@@ -17,9 +17,9 @@ int main(void)
 	while (1)
 	{
 		wait1Sec();
-		*DUART_OPS = (char) 0xFC;
+		*DUART_OPS = (char) 0x04;
 		wait1Sec();
-		*DUART_OPR = (char) 0xFC;
+		*DUART_OPR = (char) 0x04;
 	}
 }
 
