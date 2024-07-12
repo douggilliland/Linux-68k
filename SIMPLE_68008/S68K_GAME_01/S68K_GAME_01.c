91a2e23b71087c0c9deba39a869459a5ede2dbd3
Doug Gilliland <doug@douglasgilliland.com>
2024-07-12 15:49:48 -0400

    Update S68K_GAME_01.c

--- SIMPLE_68008/S68K_GAME_01/S68K_GAME_01.c
+++ SIMPLE_68008/S68K_GAME_01/S68K_GAME_01.c
@@ -13,49 +13,15 @@ int main(void)
 	unsigned char * DUART_OPC = (unsigned char *) DUART_OPC_ADR;	/* Output port config (W)	*/
 	*DUART_OPC = 0x0;
 	
-	printString("Turn on LED for a second\n\r");
-	setLED(1);
-	printString("Type a string\n\r");
+	printString("Guess a number from 1 to 99\n\r");
 	lenStr = getString(inStr);
-	printString("\n\r");
-	setLED(0);
-	if (lenStr > 0)
-		printString("String non-zero length\n\r");
+	iRtn = strToNum(inStr);
+	if (iRtn > 500)
+		printString("Number is too high\n\r");
+	else if (iRtn < 500)
+		printString("Number is too low\n\r");
 	else
-		printString("String was zero length\n\r");
-	printString(inStr);
-	printString("\n\r");
-	wait1Sec();
-	printString("Test String to number\n\r");
-	iRtn = strToNum("12345");
-	if (iRtn == 12345)
-		printString("string to number = OK\n\r");
-	else
-		printString("str to num BAD\n\r");
-	intToStr(123456, inStr);
-	printString(inStr);
-	while (1)
-	{
-		rxChar = getCharA();
-		putCharA(rxChar);
-	}
-}
-
-void setLED(unsigned char LEDVal)
-{
-	unsigned char * DUART_OPS = (unsigned char *) DUART_OPS_ADR;	/* Output port Set (W)		*/
-	unsigned char * DUART_OPR = (unsigned char *) DUART_OPR_ADR;	/* Output port Clear (W)	*/
-	if (LEDVal == 0)
-		*DUART_OPS = 0x04;
-	else
-		*DUART_OPR = 0x04;
-}
-
-void wait1Sec(void)
-{
-	unsigned long loopCt = 50000;
-	while (loopCt > 0)
-		loopCt -= 1;
+		printString("Good job, you got it\n\r");
 }
 
 #include "S68K_Serial.h"
