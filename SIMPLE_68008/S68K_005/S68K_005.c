17f07d76860d532a5a88070ca4769b1ee0929a19
Doug Gilliland <doug@douglasgilliland.com>
2024-07-12 15:29:02 -0400

    Add comments

--- SIMPLE_68008/S68K_005/S68K_005.c
+++ SIMPLE_68008/S68K_005/S68K_005.c
@@ -4,21 +4,6 @@
 #include "SIMPLE_68008.h"
 #include "main.h"
 
-/* Function Prototypes go here for compiler warning purposes	*/
-
-/* void wait1Sec(void);
-void setLED(unsigned char LEDVal);
-int strlen(char *);
-int getString(char *);
-void printString(char * pStr);
-int isStrNum(char *);
-int strToNum(char *);
-void intToStr(int, char*);
-char getCharA(void);
-char getCharB(void);
-void putCharA(char);
-void putCharB(char); */
-
 int main(void)
 {
 	int lenStr;
--- SIMPLE_68008/S68K_005/main.h
+++ SIMPLE_68008/S68K_005/main.h
@@ -1,15 +1,19 @@
 /* main.h		*/
 /* Function Prototypes go here for compiler warning purposes	*/
 
+#ifndef MAIN_h
 void wait1Sec(void);
 void setLED(unsigned char LEDVal);
+/* From S68K_Strings.h	*/
 int strlen(char *);
 int getString(char *);
 void printString(char * pStr);
 int isStrNum(char *);
 int strToNum(char *);
 void intToStr(int, char*);
+/* From S68K_Serial.h */
 char getCharA(void);
 char getCharB(void);
 void putCharA(char);
 void putCharB(char);
+#endif
