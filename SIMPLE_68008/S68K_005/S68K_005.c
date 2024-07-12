143892441fb43344ca14f25485f30162bf90637d
Doug Gilliland <doug@douglasgilliland.com>
2024-07-12 15:22:51 -0400

    Move fcn prototypes to main.h file

--- SIMPLE_68008/S68K_005/S68K_005.c
+++ SIMPLE_68008/S68K_005/S68K_005.c
@@ -1,10 +1,12 @@
 /* S68K_005 */
+/* My weird way of using the #includes at the end of the file	*/
 
 #include "SIMPLE_68008.h"
+#include "main.h"
 
-/* Function Prototypes go here for compiler warning purposes */
+/* Function Prototypes go here for compiler warning purposes	*/
 
-void wait1Sec(void);
+/* void wait1Sec(void);
 void setLED(unsigned char LEDVal);
 int strlen(char *);
 int getString(char *);
@@ -15,7 +17,7 @@ void intToStr(int, char*);
 char getCharA(void);
 char getCharB(void);
 void putCharA(char);
-void putCharB(char);
+void putCharB(char); */
 
 int main(void)
 {
@@ -54,108 +56,6 @@ int main(void)
 	}
 }
 
-/*
-void intToStr(int num, char* str)
-{
-    int i = 0;
-    int is_negative = 0;
-    
-    if (num < 0)
-	{
-        is_negative = 1;
-        num = -num;
-    }
-    
-    do 
-	{
-        str[i++] = num % 10 + '0';
-        num /= 10;
-    } while (num > 0);
-    
-    if (is_negative) 
-	{
-        str[i++] = '-';
-    }
-    str[i] = '\0';
-    
-    int j = 0;
-    while (j < i/2) 
-	{
-        char temp = str[j];
-        str[j] = str[i-j-1];
-        str[i-j-1] = temp;
-        j++;
-    }
-}
-
-int strToNum(char * pStr)
-{
-	int numRtn = 0;
-	int offset;
-	if (isStrNum(pStr) == 0)
-		return numRtn;
-	for (offset = 0; offset < strlen(pStr); offset++)
-	{
-		numRtn = numRtn * 10;
-		numRtn += (pStr[offset] - '0');
-	}
-	return numRtn;
-}
-
-int isStrNum(char * strPtr)
-{
-	int offset;
-	int len;
-	len = strlen(strPtr);
-	for (offset=0; offset<len; offset++)
-	{
-		if (strPtr[offset] > '9')
-			return 0;
-		if (strPtr[offset] < '0')
-			return 0;
-	}
-	return 1;
-}
-
-int getString(char * strPtr)
-{
-	int strLen = 0;
-	char rxChar;
-	int endFlag = 0;
-	while (endFlag == 0)
-	{
-		rxChar = getCharA();
-		*strPtr = rxChar;
-		strPtr += 1;
-		strLen += 1;
-		endFlag = 0;
-		if (strLen > 78)
-			endFlag = 1;
-		else if (rxChar == '\n')
-			endFlag = 1;
-		else if (rxChar == '\r')
-			endFlag = 1;
-		putCharA(rxChar);
-	}
-	*strPtr = 0;
-	return strLen;
-}
-
-void printString(char * pStr)
-{
-	for (unsigned int cc = 0; cc < strlen(pStr); cc++)
-		putCharA(pStr[cc]);
-}
-
-int strlen(char * strToMeasure)
-{
-	int ct = 0;
-	while (strToMeasure[ct] != 0)
-		ct += 1;
-	return ct;
-}
-*/
-
 void setLED(unsigned char LEDVal)
 {
 	unsigned char * DUART_OPS = (unsigned char *) DUART_OPS_ADR;	/* Output port Set (W)		*/
@@ -173,58 +73,5 @@ void wait1Sec(void)
 		loopCt -= 1;
 }
 
-/*
-char getCharA(void)
-{
-	unsigned char * DUART_SRA = (unsigned char *) DUART_SRA_ADR;
-	unsigned char * DUART_RBA = (unsigned char *) DUART_RBA_ADR;
-	unsigned char stat = 0;
-	while (stat == 0)
-	{
-		stat = *DUART_SRA;
-		stat &= 1;
-	}
-	return (*DUART_RBA);
-}
-
-char getCharB(void)
-{
-	unsigned char * DUART_SRB = (unsigned char *) DUART_SRB_ADR;
-	unsigned char * DUART_RBB = (unsigned char *) DUART_RBB_ADR;
-	unsigned char stat = 0;
-	while (stat == 0)
-	{
-		stat = *DUART_SRB;
-		stat &= 1;
-	}
-	return (*DUART_RBB);
-}
-
-void putCharA(char outChar)
-{
-	unsigned char * DUART_SRA = (unsigned char *) DUART_SRA_ADR;
-	unsigned char * DUART_TBA = (unsigned char *) DUART_TBA_ADR;
-	char stat = 0;
-	while (stat == 0)
-	{
-		stat = *DUART_SRA;
-		stat &= 4;
-	}
-	*DUART_TBA = outChar;
-}
-
-void putCharB(char outChar)
-{
-	unsigned char * DUART_SRB = (unsigned char *) DUART_SRB_ADR;
-	unsigned char * DUART_TBB = (unsigned char *) DUART_TBB_ADR;
-	char stat = 0;
-	while (stat == 0)
-	{
-		stat = *DUART_SRB;
-		stat &= 4;
-	}
-	*DUART_TBB = outChar;
-}
-*/
 #include "S68K_Serial.h"
 #include "S68K_Strings.h"
