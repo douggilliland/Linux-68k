a490f6faf1f6c399f4984ac6f49094b0bbc5bbe8
Doug Gilliland <doug@douglasgilliland.com>
2024-07-23 09:56:51 -0400

    Update S68K_008.s

--- SIMPLE_68008/S68K_008/S68K_008.s
+++ SIMPLE_68008/S68K_008/S68K_008.s
@@ -31,18 +31,20 @@ INU   = 26  | Input port (unlatched)    (R)
 OPS   = 28  | Output port Set           (W)
 STC   = 28  | Start Counter             (R)
 OPR   = 30  | Output port Clear         (W)
-SPC   = 30  | Stop Counter	          (R)
+SPC   = 30  | Stop Counter	            (R)
 
 DUART_Vect = 0x100
 DUART_VR = DUART_Vect / 4
 BIG_CTR = 0x604
+INTRTN = 0x1200
 
 	.ORG	CODE_START
     movem.l %d0/%a0-%a1, -(%SP)	| Save changed registers
 	ori.w	#0x0700, %sr		| Disable interrupts
+	move.l	#0x0, BIG_CTR		| Clear the big counter
 	| Fill the interrupt vector table entry for DUART interrupt
 	movea.l	#DUART_Vect, %a0
-	move.l	#0x1200, %d0
+	move.l	#INTRTN, %d0
 	move.l	%d0, (%a0)
 	move.b 	#DUART_VR, %d0
 	| Set DUART interrupt vector
@@ -50,26 +52,25 @@ BIG_CTR = 0x604
 	move.b	%d0, IVR(%a0)		| Interrupt Vector Register
 	move.b	ACR(%a0), %d0		| Read ACR
 	andi.b	#0x8f, %d0			| Mask ACR bits
-	ori.b	#0x30, %d0			| XTAL X1, X2
+	ori.b	#0x70, %d0			| Timer mode using XTAL X1, X2 dive by 16
 	move.b	%d0, ACR(%a0)		| Write back ACR
 	move.b	#0x0F, CTU(%a0)		| Write Counter Upper
 	move.b	#0x00, CTL(%a0)		| Write Counter Lower
 	move.b	STC(%a0), %d0		| Start Counter
 	| Set DUART interrupt mask to enable Counter/Timer interrupt
 	move.b	#0x08, IMR(%a0)		| Interrupt Mask Register
-	move.l	#0x0, BIG_CTR		| Clear the big counter
 	andi.w	#0xF8FF, %sr		| Enable interrupts
     movem.l (%SP)+, %d0/%a0-%a1	| Restore registers
 	rts
 	
-	.ORG	0x1200
+	.ORG	INTRTN
 IntLev2:
     movem.l %d0/%a0, -(%SP)     | Save changed registers
-	move.b	SPC(%a0), %d0		| Stop Counter with dummy read
+	move.b	SPC(%a0), %d0		| Stop Counter with dummy read clears int
 	movea.l	#DUART, %a0			| DUART base address
 	move.b	#0x0F, CTU(%a0)		| Write Counter Upper
 	move.b	#0x00, CTL(%a0)		| Write Counter Lower
-	move.b	STC(%a0), %d0		| Start Counter with dummy read
+|	move.b	STC(%a0), %d0		| Start Counter with dummy read enables int
 	addi.l	#1, BIG_CTR			| Increment the big counter
     movem.l (%SP)+, %d0/%a0		| Restore registers
 	rte
