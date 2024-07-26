1b7d7b8ee3dbada696d579973f097ba14a47005a
Doug Gilliland <doug@douglasgilliland.com>
2024-07-26 11:29:39 -0400

    Update main.h

--- SIMPLE_68008/S68K_GAME_03/main.h
+++ SIMPLE_68008/S68K_GAME_03/main.h
@@ -2,19 +2,19 @@
 /* Function Prototypes go here for compiler warning purposes	*/
 
 #ifndef MAIN_h
-int playGame(void);
+/* int playGame(void); */
 /* From S68K_Strings.h	*/
-int strlen(char *);
+/* int strlen(char *);
 int getString(char *);
 void printString(char * pStr);
 int isStrNum(char *);
 int strToNum(char *);
 void intToStr(int, char*);
-void printInt(int);
+void printInt(int); */
 /* From S68K_Serial.h */
-char getCharA(void);
+/* char getCharA(void);
 char getCharB(void);
 void putCharA(char);
 void putCharB(char);
-int makeSeedFromKeyWait(void);
+int makeSeedFromKeyWait(void); */
 #endif
