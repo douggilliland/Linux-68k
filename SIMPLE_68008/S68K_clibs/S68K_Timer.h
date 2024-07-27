b62b4899df7095891edecbf01df1f87cc31f7774
Doug Gilliland <doug@douglasgilliland.com>
2024-07-27 18:50:02 -0400

    stuffs

---
+++ SIMPLE_68008/S68K_clibs/S68K_Timer.h
@@ -0,0 +1,22 @@
+/* S68K_Timer.h
+Timer functions */
+
+#ifndef S68K_TIMER_h
+#include "SIMPLE_68008.h"
+
+/* Function Prototypes go here for compiler warning suppression purposes */
+
+
+/* Globals go here */
+
+
+/* Functions go here */
+
+void init_Timer(void)
+{
+	
+}
+
+#else
+#define S68K_TIMER_h
+#endif
